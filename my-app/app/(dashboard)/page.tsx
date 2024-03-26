a4a50dff4fb6e9d6729bca4721c271dc93ed5fcc
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-03-26 14:24:45 -0300

    feat: mongoose and mongoDB installation, creation models.

--- my-app/app/(dashboard)/page.tsx
+++ my-app/app/(dashboard)/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Cards } from "../_components/_dashboard-layout/Cards";
 import Heading from "../_components/heading";
+import { CollectionsForm } from "../_components/_collections/CollectionsForm";
 
 export default function Home() {
   return (
     <main>
-      <Heading title="dashboard" subTitle="overview your store" />
-      <Cards />
+      {/*   <Heading title="dashboard" subTitle="overview your store" />
+      <Cards /> */}
+      <CollectionsForm />
     </main>
   );
 }
--- my-app/app/_components/_collections/CollectionsForm.tsx
+++ my-app/app/_components/_collections/CollectionsForm.tsx
@@ -1,3 +1,122 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import Heading from "../heading";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string(),
+});
+
 export const CollectionsForm = () => {
-  return <div className="p">CollectionsForm</div>;
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <section className="h-full w-full">
+      <Heading title="Collections" subTitle="Create your collecions" />
+      <div className="flex items-center justify-center">
+        <Card className="w-[650px] shadow-lg">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CardContent className="py-5">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name new collection</FormLabel>
+                      <FormControl>
+                        <Input placeholder="New collection" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem className="my-5">
+                      <FormLabel>Describe your collection</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Type your description here."
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="my-5 grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="picture">Picture</Label>
+                  <Input id="picture" type="file" />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div className="flex items-center gap-4">
+                  <Button type="submit" variant="outline">
+                    Submit
+                  </Button>
+                  <Button
+                    type="button"
+                    variant="destructive"
+                    onClick={() => router.push("/collections")}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </CardFooter>
+            </form>
+          </Form>
+        </Card>
+      </div>
+    </section>
+  );
 };
+
+/* 
+<Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+ */
