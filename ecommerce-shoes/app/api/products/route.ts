aa94439bdb45698669790c184073faeb009deb36
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-04-08 10:08:59 -0300

    feat: initial routes

---
+++ ecommerce-shoes/app/api/products/route.ts
@@ -0,0 +1,82 @@
+import { auth } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+export async function CreateProduct(req: NextRequest) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const {
+      title,
+      description,
+      media,
+      category,
+      collections,
+      tags,
+      sizes,
+      colors,
+      price,
+      expense,
+    } = await req.json();
+
+    if (!title || !description || !media || !category || !price || !expense) {
+      return new NextResponse("Not enough data to create a product", {
+        status: 400,
+      });
+    }
+
+    const newProduct = await prisma.product.create({
+      data: {
+        title,
+        description,
+        media,
+        category,
+        collections,
+        tags,
+        sizes,
+        colors,
+        price,
+        expense,
+      },
+    });
+
+    await newProduct.save();
+
+    if (collections) {
+      for (const collectionId of collections) {
+        const collection = await Collection.findById(collectionId);
+        if (collection) {
+          collection.products.push(newProduct._id);
+          await collection.save();
+        }
+      }
+    }
+
+    return NextResponse.json(newProduct, { status: 200 });
+  } catch (err) {
+    console.log("[products_POST]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export const GET = async (req: NextRequest) => {
+  try {
+    await connectToDB();
+
+    const products = await Product.find()
+      .sort({ createdAt: "desc" })
+      .populate({ path: "collections", model: Collection });
+
+    return NextResponse.json(products, { status: 200 });
+  } catch (err) {
+    console.log("[products_GET]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
+
+export const dynamic = "force-dynamic";
