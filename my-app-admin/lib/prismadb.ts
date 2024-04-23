e74ad6bcd094989c0c06cb90aa4cabf45aebc372
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-04-23 10:58:49 -0300

    feat: create user

--- my-app-admin/lib/prismadb.ts
+++ my-app-admin/lib/prismadb.ts
@@ -1,27 +1,10 @@
-/* import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const prismadb = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prismadb
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prismadb
- */
-
  import { PrismaClient } from '@prisma/client';
 
 declare global {
-    var prisma:PrismaClient | undefined;
+   var prisma:PrismaClient | undefined;
 }
 
-const prismadb = new PrismaClient();
+const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb;
 
 export default prismadb; 
