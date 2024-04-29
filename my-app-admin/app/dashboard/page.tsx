b39cbc2fe7f4e0b10aa47a516451c0d4ef68eca1
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-04-29 10:42:40 -0300

    feat: callbacks

--- my-app-admin/app/dashboard/page.tsx
+++ my-app-admin/app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default async function DashboardPage() {
   const session = await auth();
+
+  
   return (
     <div>
       {JSON.stringify(session)}
--- my-app-admin/auth.ts
+++ my-app-admin/auth.ts
@@ -1,16 +1,41 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prismadb from "./lib/prismadb"
-import Google from "next-auth/providers/google"
-import Facebook from "next-auth/providers/facebook"
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prismadb from "./lib/prismadb";
+import { UserRole } from "@prisma/client";
+import { getUserById } from "./data/user";
+import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
 
-export const { handlers: { GET, POST }, 
-auth,
-signIn,
-signOut,
- } = NextAuth({
-   adapter: PrismaAdapter(prismadb),
-   session: { strategy: "jwt" },
-   ...authConfig,
- })
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  callbacks: {
+       async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(prismadb),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
